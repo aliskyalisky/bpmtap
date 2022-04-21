2cc51f4fb409811ed8bfe4baa25f8dcc45e09bea
aliskyalisky <aleksi.liski@gmail.com>
2022-04-21 20:46:37 +0300

    Nappi

---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+      <div className="Button">
+          <button type="button" className="tapbtn"></button>
+      </div>
+    );
+  }
+  
+  export default Button;
